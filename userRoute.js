d83589c2032bce41d0858e59c0381bc60b5b2b43
Eliseo Leon <45862604+eliseoleon@users.noreply.github.com>
2020-03-22 22:41:29 -0600

    Delete userRoute.js

--- userRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const user = require('../controllers/userController')
-const app = express();
-
-//Ruta en la cual accederemos a crear el usuario
-app.post('/user/create', user.create)
-
-module.exports = app;
