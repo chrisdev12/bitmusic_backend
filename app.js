10402b1196ccefb68a941e7c9905f4072f26338d
Eliseo Leon <45862604+eliseoleon@users.noreply.github.com>
2020-03-22 19:37:45 -0600

    Add files via upload
    Integración del modulo de creación de usuario

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const musicRoute = require('./routes/songs');
+const userRoute = require('./routes/userRoute');
 const app = express();
 
 app.use(bodyParser.json());
 app.use('/api',musicRoute); //Rutas relacionadas con la música
+app.use('/api',userRoute); //Rutas relacionada para el usuario
 
 module.exports = app;
