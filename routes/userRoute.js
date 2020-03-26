023284bcdf96affc562e6f3a216c8b4e746b52b8
Eliseo Leon <45862604+eliseoleon@users.noreply.github.com>
2020-03-26 07:42:12 -0600

    Add files via upload
    Archivo el cual ya contiene la ruta para actualizar el usuario

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,6 +3,7 @@ const user = require('../controllers/userController')
 const app = express();
 
 //Ruta en la cual accederemos a crear el usuario
-app.post('/user/create', user.create)
-
+app.post('/user/create', user.create),
+//Ruta en la cual podremos acceder al metodo de actualizar
+app.put('/user/update/:id', user.update)
 module.exports = app;
